2073c8d3b60db117463f7dd03fd47f61c18cd81d
Emmanuel-Anaba <emmaanaba29@gmail.com>
2024-05-03 13:03:51 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Section from "./components/Section";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Section className="">
+        <p></p>
+      </Section>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Section.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react/prop-types */
+const Section = ({ children, className }) => {
+  return <section className={className}>{children}</section>;
+};
+
+export default Section;
